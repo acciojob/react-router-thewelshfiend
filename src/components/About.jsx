38080b9c0cb6a33e5595727bbb5ba6e914204c02
Kiran TJ <kiran.thomas.jacob@gmail.com>
2024-10-07 13:53:26 +0530

    Commit 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div>
             <h1>About</h1>
-            <p>This is a sample react router program.</p>
+            <p>This is a sample React Router program.</p>
         </div>
     )
 }

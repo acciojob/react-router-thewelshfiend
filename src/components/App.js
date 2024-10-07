d447406d1b79aee29c04ffe352321fef5d1eeb76
Kiran TJ <kiran.thomas.jacob@gmail.com>
2024-10-07 13:47:09 +0530

    Commit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 
 import React from "react";
 import './../styles/App.css';
+import Switch from "./Switch";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    return (
+        <BrowserRouter>
+            <div>
+                {/* Do not remove the main div */}
+                <Switch />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App
